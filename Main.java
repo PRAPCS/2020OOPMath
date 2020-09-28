97fe0917c75817549e0f4c1ad72b42a341aee12a
Mr. Burger <bburger@d155.org>
2020-09-28 19:08:38 +0000

    Examples

--- Main.java
+++ Main.java
@@ -2,7 +2,13 @@ class Main {
   public static void main(String[] args) {
   
   //https://introcs.cs.princeton.edu/java/11precedence/
-  System.out.println();
-
+  System.out.println(5+10/2*4);
+  System.out.println((5+10)/2*4);
+  System.out.println((int)5.5+100);
+  System.out.println((int)(5.5+5.8));
+  System.out.println((int)(Math.random()*10));
+  System.out.println((double)5+100);
+  System.out.println(5+10%2*4);
+  System.out.println(7.5%2);
   }
 }
